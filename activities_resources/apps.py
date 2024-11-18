f4e095321d7c2957a152334a73a9301b4ac10ea8
brandondiaz <brandon.diaz.dev@gmail.com>
2024-11-18 03:21:06 -0600

    Initial commit: set up project structure and added basic configurations
    - Added requirements.txt with initial dependencies.
    - Created the following apps: activities_resources, results, tests, users.
    - Configured .gitignore to exclude unnecessary files.

---
+++ activities_resources/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ActivitiesResourcesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'activities_resources'
---
+++ mentescom/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mentescom project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mentescom.settings')
+
+application = get_asgi_application()
